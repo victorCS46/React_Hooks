ae6d9a604cfbdf6eb29a29558806db1cc6a97b08
victorCS46 <victorcruzsuero@gmail.com>
2019-10-17 15:09:13 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [search, setSearch] = useState("");
+  const [items, setItems] = useState([]);
+  const [submit, setSubmit] = useState(false);
+  
+  const handleChange = (e) =>{
+    e.preventDefault();
+    setSearch(e.target.value);
+    // console.log(search);
+  } 
+
+   const handleSubmit = async (e) =>{
+    e.preventDefault();
+    const response = await axios.get(`https://api.github.com/search/repositories?q=${search}&per_page=6`);
+    setItems(response.data.items);
+    console.log(response.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Que deseas buscar?" onChange={handleChange}/>
+        <button>Search</button>
+      </form>
     </div>
   );
 }
-
 export default App;
