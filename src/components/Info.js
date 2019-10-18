087a56d50bb4479bf41fccf5295ee60942a59193
victorCS46 <victorcruzsuero@gmail.com>
2019-10-18 16:54:53 -0400

    Añadiendo estilo al search component

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,7 +5,8 @@ function Info(props){
     const x = props.items.map(item =>(
         <div key={item.id}>
             <div><img className="vcs-img" alt="No img" src={item.owner.avatar_url}></img></div>
-            <div><label>{item.name}</label></div>
+            <div><label>{item.full_name}</label></div>
+            <div><label>{item.language}</label></div>
         </div>
     ));
     
